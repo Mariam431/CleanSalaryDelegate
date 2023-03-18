d5a40ba78d0c888da53c29af5bdea8e4cc861d2f
mariam <mariam@MYPROBOOK>
2023-03-18 14:21:15 +0400

    Add project files.

---
+++ CleanSalaryDelegate/Delegate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanSalaryDelegate
+{
+    internal class Delegate
+    {
+        public double dirtySalary { get; set; }
+        public Delegate(out double dirtySalary)
+        {
+            Console.Write("Enter dirty salary: ");
+            dirtySalary = double.Parse(Console.ReadLine());
+            this.dirtySalary = dirtySalary;
+        }
+
+        public delegate void DelegateName(out double y); //Delegate
+
+
+        public void MilFee(out double cleanSalary)
+        {
+            double militaryFee = 0.1 * dirtySalary;
+            cleanSalary = dirtySalary - militaryFee;
+        }
+
+
+
+        public void Tax(out double cleanSalary )
+        {
+            double taxes = 0.05 * dirtySalary;
+            cleanSalary = dirtySalary - taxes;
+        }
+
+    }
+}
---
+++ CleanSalaryDelegate/Program.cs
@@ -0,0 +1,23 @@
+﻿using static CleanSalaryDelegate.Delegate;
+namespace CleanSalaryDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Delegate ClassObj = new Delegate(out double x);
+            DelegateName delegateName = new DelegateName(ClassObj.MilFee);
+            delegateName += ClassObj.Tax;
+            delegateName(out double y);
+
+            
+            Console.WriteLine($"Clean salary: {y}");
+        }
+        
+
+    }
+}
+
+
+
+
